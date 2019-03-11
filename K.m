e4789f9fe6c2726e5c69ed4ad3696b53360b65e5
CesarMorenoG <48446667+CesarMorenoG@users.noreply.github.com>
2019-03-11 11:39:55 -0500

    Delete K.m

--- K.m
+++
@@ -1,8 +0,0 @@
-function [ Kpar ] = K( N,kx,ky,i,j,nx,ny,Lcx,Lcy )
-
-syms x y 
-
-Kpar=int(int(kx*diff(N(i),x)*diff(N(j),x)+ky*diff(N(i),y)*diff(N(j),y),x,Lcx(nx),Lcx(nx+1)),y,Lcy(ny),Lcy(ny+1));
-
-end
-
